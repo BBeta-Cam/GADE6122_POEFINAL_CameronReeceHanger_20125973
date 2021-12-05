d4b475e01e2f49ec857700648a4169f187a2ec45
BBeta-Cam <91014480+BBeta-Cam@users.noreply.github.com>
2021-12-05 12:34:44 +0200

    Added Leader Get and Set

--- GADE6122_POE_CameronReeceHanger_20125973/GADE6122_POE_CameronReeceHanger_20125973/Map.cs
+++ GADE6122_POE_CameronReeceHanger_20125973/GADE6122_POE_CameronReeceHanger_20125973/Map.cs
@@ -43,6 +43,12 @@ namespace GADE6122_POE_CameronReeceHanger_20125973
             get { return MAGE; }
             set { mage = value; }
         }
+        
+        public List<Enemy> LEADER
+        {
+            get { return LEADER; }
+            set { leader = value; }
+        }
 
         private int mapWidth;
 
